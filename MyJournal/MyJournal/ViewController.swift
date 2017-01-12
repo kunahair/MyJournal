0d5ce73d2bc9e1fb2addaf775711bfc8a00f1b34
Weizhuo Tian <tianweizhuo@gmail.com>
2017-01-12 23:29:55 +1100

    Edit Page Layout

--- MyJournal/MyJournal/ViewController.swift
+++ MyJournal/MyJournal/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollView.contentSize.height = 1000
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+ 
+   }
 
