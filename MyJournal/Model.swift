15eb17f30c094c87cf724cf004025a866ff303c7
Josh Gerlach <s3453952@student.rmit.edu.au>
2017-01-14 21:54:31 +1100

    Moved Weather and Location calls from init() to their own User accessible functions.

--- MyJournal/Model.swift
+++ MyJournal/Model.swift
@@ -15,17 +15,32 @@ class Model{
     {
         
         //The Latitude of rmit university is -37.8087. The Longitude of rmit university is 144.9637
-        
+    }
+    
+    /**
+     User accesible function to get the weather from API
+     Returns a Weather Object
+    **/
+    func getWeather(lat:Float, lon:Float)->Weather
+    {
         //Get current weather from OpenWeatherMap API and add to current Weather Object
         let openWeatherMap = OpenWeatherMap()
-        let currentWeather:Weather = openWeatherMap.getCurrentWeather(lat: 140.0, lon: 137.0)
-        
+        return openWeatherMap.getCurrentWeather(lat: 140.0, lon: 137.0)
+    }
+    
+    /**
+     User accessible function to get a User Readable location
+     Returns a Location Object
+    **/
+    func getReadableAddress(lat:Float, lon:Float)->Location
+    {
         //Get User Readable Address from OpenStreet API and load into Location Object
         let openStreetMap = OpenStreetMap()
         var location:Location = Location()
+        location.lat = lat
+        location.lon = lon
         location.address = openStreetMap.getReadableAddress(lat: -37.8087, lon: 144.9637)
-        
-        print(currentWeather.description)
+        return location
     }
     
 }
